2b05fac72bf4e2d59382f2b2c25c53cb7fc3ab31
John Grubbs <johnmlowery@gmail.com>
2018-07-26 18:23:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,8 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 
 function accessElementInArray(array, index){
-  [array(index[3])]
-  return array
+  console.log (array[index])
 }
 
 
